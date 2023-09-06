b79bea50b6b17edf2866f36e8f5162627a4f4bb7
Munachi Iwundu <realjones2012@gmail.com>
2023-09-06 01:16:21 +0000

    Task 8: 8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for ch in s:
+        if 'a' <= ch <= 'z':
+            ch_upper = ord(ch) - ord('a') + ord('A')
+            print("{}".format(chr(ch_upper)), end="")
+        else:
+            print("{}".format(ch), end="")
+    print("")
