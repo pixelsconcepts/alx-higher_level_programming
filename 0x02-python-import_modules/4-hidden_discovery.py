85d7f5d2a64df2cd7af82638f96d61ab4840791d
Munachi Iwundu <realjones2012@gmail.com>
2023-09-10 21:47:20 +0000

    4. Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+    filtered_names = [name for name in names if not name.startswith("__")]
+
+    for name in filtered_names:
+        print(name)
