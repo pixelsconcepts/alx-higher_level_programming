9c1a134c4e0ef3a6025ddedeb0a2f409e7eb6171
Munachi Iwundu <realjones2012@gmail.com>
2023-09-11 23:04:12 +0000

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     char_list = list(my_string)
-
     char_list = [char for char in char_list if char.lower() != 'c']
-
     result = ''.join(char_list)
-    
-    return result 
+    return result
