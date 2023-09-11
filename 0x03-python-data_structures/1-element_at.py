ba0cd44ce5ae1d73dc1886deec8ef548056bda67
Munachi Iwundu <realjones2012@gmail.com>
2023-09-11 21:25:07 +0000

    correct task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0:
         return None
-    for i in my_list:
-        if i == idx:
-            return my_list[idx]
+    if idx > len(my_list):
+        return None
+    return my_list[idx]
