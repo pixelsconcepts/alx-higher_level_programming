f8a23adf608b1c154d79466a23a61cdcf8829df5
Munachi Iwundu <realjones2012@gmail.com>
2023-09-11 22:42:53 +0000

    4. Replace in a copy

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     if len(my_list) < 0:
-        return None
+        return
     for i in my_list:
         print("{:d}".format(i))
