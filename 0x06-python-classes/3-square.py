6dd7ec3f4a6da4a3bffd2a2c710043d951e74e02
Munachi Iwundu <realjones2012@gmail.com>
2023-09-26 23:35:43 +0000

    Task 3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+a class Square that defines a square by
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """
+    An object of square
+    """
+    def __init__(self, size=0):
+        """
+        Innitializing the Square object
+        Args:
+            param2 = size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
