b0197af5a457d620f22945c4d637b446e63f83e1
Munachi Iwundu <realjones2012@gmail.com>
2023-09-26 00:49:52 +0000

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,9 @@
 def safe_print_division(a, b):
     result = 0
     try:
-        result = int(a) / int(b)
+        result = a / b
     except ZeroDivisionError:
         return None
     finally:
         print("Inside result: {}".format(result))
-
     return result
