587917020a12cf2a2e2f87610618836746cf5537
Munachi Iwundu <realjones2012@gmail.com>
2023-09-26 00:07:06 +0000

    Task 2 correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-    except:
+    except IndexError:
         raise
     print()
     return count
