9c09507f200c620fa4ac6e6fb47f93c9605de9be
Munachi Iwundu <realjones2012@gmail.com>
2023-09-26 22:49:05 +0000

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+a class Square that defines a square by
+
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """
+    An object of square
+    """
+    def __init__(self, size):
+
+        """
+        Innitializing the Square object
+        Args:
+            param1 = self
+            param2 = size
+        """
+        self.__self = size
