a8001e4ecf7e92f7dfe18dddddbcd8a6653cf9fe
Munachi Iwundu <realjones2012@gmail.com>
2023-10-10 14:16:48 +0000

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+rite a function that returns the dictionary description 
+with simple data structure
+"""
+
+def class_to_json(obj):
+    """
+    Convert an instance of a class to a dictionary with
+    serializable attributes.
+
+    Parameters:
+    - obj: An instance of a class.
+
+    Returns:
+    - dict: A dictionary containing serializable attributes of the object.
+    """
+    attributes = obj.__dict__
+
+    serializable_attributes = {
+        key: value
+        for key, value in attributes.items()
+        if isinstance(value, (list, dict, str, int, bool))
+    }
+
+    return serializable_attributes
