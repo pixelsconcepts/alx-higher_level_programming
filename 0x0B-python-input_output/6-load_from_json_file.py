4d57d7b43de8b98dde4af9fed621e94402e610a8
Munachi Iwundu <realjones2012@gmail.com>
2023-10-10 13:47:23 +0000

    task8

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that creates an Object from a “JSON file”:
+"""
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+
+    return data
+
