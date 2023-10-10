d438521e53b5dc17fca93fc5c295003c229104d2
Munachi Iwundu <realjones2012@gmail.com>
2023-10-10 23:06:18 +0000

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Write a class Student that defines a student by:
+-first_name: Public instance attributes
+-last_name: Public instance attributes
+-age: Public instance attributes
+"""
+
+
+class Student:
+    """
+        Initialize a Student instance.
+
+        Parameters:
+        - first_name (str): The first name of the student.
+        - last_name (str): The last name of the student.
+        - age (int): The age of the student.
+        """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+
+        Parameters:
+        - attrs (list, optional): A list of attribute names to include
+        in the dictionary.
+          If not provided, all attributes will be included.
+
+        Returns:
+        dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            attrs = vars(self).keys()
+        else:
+            attrs = [attr for attr in attrs if hasattr(self, attr)]
+
+        student_dict = {attr: getattr(self, attr) for attr in attrs}
+
+        return student_dict
