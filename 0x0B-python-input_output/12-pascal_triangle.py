ca34ecf52cf43149d8c5fc49601b928700a1fe73
Munachi Iwundu <realjones2012@gmail.com>
2023-10-10 23:35:34 +0000

    task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Parameters:
+    - n (int): The number of rows to generate.
+
+    Returns:
+    list: A list of lists representing Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
