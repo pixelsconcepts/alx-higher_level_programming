f4c884fe3a85420c3b62a8201954c312c0ffdc6d
Munachi Iwundu <realjones2012@gmail.com>
2023-10-10 12:21:55 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -17,4 +17,4 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as file_content:
         content = file_content.read()
 
-        print("{}".format(content))
+        print(content, end="")
