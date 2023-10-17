87d8d0cb9a7f8a1f208f730c8f64fc61bf1e9f1e
Munachi Iwundu <realjones2012@gmail.com>
2023-10-17 03:40:40 +0000

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,6 +25,36 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The new size value.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Return the string representation of the Square.
@@ -36,3 +66,25 @@ class Square(Rectangle):
         ax = self.x
         ay = self.y
         return "[Square] ({}) {}/{} - {}".format(aid, ax, ay, self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        Update the attributes of the square.
+
+        Args:
+            *args: List of arguments (non-keyworded).
+                1st argument should be the id attribute.
+                2nd argument should be the size attribute.
+                3rd argument should be the x attribute.
+                4th argument should be the y attribute.
+            **kwargs: Dictionary of keyworded arguments.
+                Each key represents an attribute to the instance.
+        """
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for attr, value in zip(attrs, args):
+                setattr(self, attr, value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+            
