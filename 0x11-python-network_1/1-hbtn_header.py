1a6feed8e0dc56109a522c66832430b5967f57b5
Munachi Iwundu <realjones2012@gmail.com>
2024-01-08 02:07:31 +0100

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,20 @@
 import urllib.request
 import sys
 
+def get_x_request_id(url):
+    try:
+        # Send a request to the URL
+        with urllib.request.urlopen(url) as response:
+            # Retrieve the value of the X-Request-Id from the headers
+            x_request_id = response.getheader('X-Request-Id')
+
+            if x_request_id:
+                print(x_request_id)
+            else:
+                print("X-Request-Id not found in the headers.")
+    except urllib.error.URLError as e:
+        print(f"Error: {e.reason}")
+
 if __name__ == "__main__":
     # Check if a URL is provided as a command line argument
     if len(sys.argv) < 2:
@@ -10,12 +24,6 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
 
-    # Send a request to the URL
-    with urllib.request.urlopen(url) as response:
-        # Retrieve the value of the X-Request-Id from the headers
-        x_request_id = response.headers.get('X-Request-Id')
+    # Call the function to retrieve and display X-Request-Id
+    get_x_request_id(url)
 
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id not found in the headers.")
