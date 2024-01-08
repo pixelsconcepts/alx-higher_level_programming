7d87bd76cf95a8d11e34439c561342dd1dae0015
Munachi Iwundu <realjones2012@gmail.com>
2024-01-08 01:50:34 +0100

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,15 +9,12 @@ if len(sys.argv) < 2:
 
 url = sys.argv[1]
 
-try:
-    # Send a request to the URL
-    with urllib.request.urlopen(url) as response:
-        # Retrieve the value of the X-Request-Id from the headers
-        x_request_id = response.headers.get('X-Request-Id')
+# Send a request to the URL
+with urllib.request.urlopen(url) as response:
+    # Retrieve the value of the X-Request-Id from the headers
+    x_request_id = response.headers.get('X-Request-Id')
 
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id not found in the headers.")
-except urllib.error.URLError as e:
-    print(f"Error: {e.reason}")
+    if x_request_id:
+        print(x_request_id)
+    else:
+        print("X-Request-Id not found in the headers.")
