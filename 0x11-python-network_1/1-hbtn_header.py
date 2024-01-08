d39570460ca1fb3367f37c4007391feae8ffd79e
Munachi Iwundu <realjones2012@gmail.com>
2024-01-08 02:05:18 +0100

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,19 +2,20 @@
 import urllib.request
 import sys
 
-# Check if a URL is provided as a command line argument
-if len(sys.argv) < 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
+if __name__ == "__main__":
+    # Check if a URL is provided as a command line argument
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-# Send a request to the URL
-with urllib.request.urlopen(url) as response:
-    # Retrieve the value of the X-Request-Id from the headers
-    x_request_id = response.headers.get('X-Request-Id')
+    # Send a request to the URL
+    with urllib.request.urlopen(url) as response:
+        # Retrieve the value of the X-Request-Id from the headers
+        x_request_id = response.headers.get('X-Request-Id')
 
-    if x_request_id:
-        print(x_request_id)
-    else:
-        print("X-Request-Id not found in the headers.")
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found in the headers.")
