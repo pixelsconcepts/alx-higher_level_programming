5b5d9a701220bf86162287d524fa73d34da1cb8a
Munachi Iwundu <realjones2012@gmail.com>
2024-01-08 05:19:47 +0100

    new change task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
 
+
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    url = "https://swapi.co/api/people"
-    params = {"search": sys.argv[1]}
-    results = requests.get(url, params=params).json()
-
-    print("Number of results: {}".format(results.get("count")))
-    [print(r.get("name")) for r in results.get("results")]
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
